9862d6520dabff3fa40b3d098e0ef3d82bbd4fb0
Vignesh S <121417886+viki5917@users.noreply.github.com>
2023-07-24 17:16:58 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+// const newYear = "1/1/2024";
+const presentYear = new Date().getFullYear();
+const nextYear=presentYear+1;
+const newYear=`1/1/${nextYear}`
+
+const daysEl = document.querySelector('.day');
+const hourEl = document.querySelector('.hour');
+const minuteEl = document.querySelector('.minute');
+const secondEl = document.querySelector('.seconds');
+
+document.getElementById('nextyear').textContent=nextYear;
+
+function timeCountDown() {
+  const nowDate = new Date();
+  const newYearDate = new Date(newYear);
+  const totalSeconds = (newYearDate - nowDate) / 1000;
+
+  const days = Math.floor(totalSeconds / 3600 / 24);
+  const hours = Math.floor(totalSeconds / 3600) % 24;
+  const minutes = Math.floor(totalSeconds / 60) % 60;
+  const seconds = Math.floor(totalSeconds) % 60;
+
+  daysEl.textContent = formatTime(days);
+  hourEl.textContent = formatTime(hours);
+  minuteEl.textContent = formatTime(minutes);
+  secondEl.textContent = formatTime(seconds);
+}
+
+function formatTime(time) {
+  return time > 10 ? time : `${time}`;
+}
+
+timeCountDown()
+setInterval(timeCountDown, 1000);
